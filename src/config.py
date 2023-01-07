0ac2332d7a581f40619174cb6d830c32ce92275e
Syed Ibrahim Shakir <omarsrstt@gmail.com>
2023-01-07 10:48:54 +0100

    Update: Make dataset class arg

--- src/config.py
+++ src/config.py
@@ -2,22 +2,33 @@ import os
 
 
 class Config:
-    dataset = "Reddit"
-    # dataset = "iSarcasm"
-    if dataset == "iSarcasm":
-        dataset = "iSarcasmEval"
 
-    file_dir = os.path.join("..", "data", dataset)
+    def __init__(self, dataset):
+        """
+        Parameters
+        ----------
+        dataset     Select the dataset ["Reddit", "iSarcasm"]
+        """
+        if dataset == "iSarcasm" or dataset == "iSarcasmEval":
+            self.dataset = "iSarcasmEval"
+        elif dataset == "Reddit":
+            self.dataset = "Reddit"
+        else:
+            assert "Invalid dataset selected, Valid = [Reddit, iSarcasm]"
+        
+        # Path to datasets
+        file_dir = os.path.join("..", "data", dataset)
 
-    if dataset == "Reddit":
-        input_file_path = os.path.join(file_dir, "train-balanced-sarcasm.csv")
-        test_file_path = os.path.join(file_dir, "test-balanced.csv")
-        comment_col_name = "comment"
-        label_col_name = "label"
-    else:
-        input_file_path = os.path.join(file_dir, "train.csv")
-        test_file_path = os.path.join(file_dir, "test.csv")
-        comment_col_name = "tweet"
-        label_col_name = "sarcastic"
+        # Store dataset specific attributes
+        if dataset == "Reddit":
+            self.input_file_path = os.path.join(file_dir, "train-balanced-sarcasm.csv")
+            self.test_file_path = os.path.join(file_dir, "test-balanced.csv")
+            self.comment_col_name = "comment"
+            self.label_col_name = "label"
+        else:
+            self.input_file_path = os.path.join(file_dir, "train.csv")
+            self.test_file_path = os.path.join(file_dir, "test.csv")
+            self.comment_col_name = "tweet"
+            self.label_col_name = "sarcastic"
 
-    preprocessed_file_path = os.path.join(file_dir, "preprocessed.csv")
+        self.preprocessed_file_path = os.path.join(file_dir, "preprocessed.csv")

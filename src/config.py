e02a407356100bf612adb147c0eb1c201c4c5533
Zainey <zainey4@gmail.com>
2023-01-13 04:40:05 +0100

    added results and evaluation app with ConLSTM test model

--- src/config.py
+++ src/config.py
@@ -25,10 +25,12 @@ class Config:
             self.test_file_path = os.path.join(file_dir, "test-balanced.csv")
             self.comment_col_name = "comment"
             self.label_col_name = "label"
+            self.MAX_SEQ_LEN = 25
         else:
             self.input_file_path = os.path.join(file_dir, "train.csv")
             self.test_file_path = os.path.join(file_dir, "test.csv")
             self.comment_col_name = "tweet"
             self.label_col_name = "sarcastic"
+            self.MAX_SEQ_LEN = 36
 
         self.preprocessed_file_path = os.path.join(file_dir, "preprocessed.csv")
